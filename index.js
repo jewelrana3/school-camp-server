460440761c1bf0910a3233fe5dae5d10d2dce57a
jewelrana3 <juyelrana7752@gmail.com>
2023-06-16 12:35:43 +0600

    ui get api

--- index.js
+++ index.js
@@ -1,11 +1,76 @@
 const express = require('express');
 const app = express();
+require('dotenv').config()
 const cors = require('cors');
 const port = process.env.PORT || 4000;
 
 // middleware
 app.use(cors())
 app.use(express.json());
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cnbwwnw.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const popularCollection = client.db("schoolCamp").collection("popular");
+    const instructorCollection = client.db("schoolCamp").collection("instructor");
+    const cartCollection = client.db("schoolCamp").collection("cart");
+
+    app.get('/popular',async(req,res)=>{
+        const result = await popularCollection.find().toArray()
+        res.send(result)
+    });
+
+    app.get('/instructor',async(req,res)=>{
+        const result = await instructorCollection.find().toArray()
+        res.send(result)
+    })
+    // cart api
+    app.get('/cart',async(req,res)=>{
+      const email = req.query.email;
+      console.log('email',email)
+      if(!email){
+       return res.send([])
+      }
+
+      const query = {email : email}
+      const result = await cartCollection.find(query).toArray()
+      console.log('result',result)
+      res.send(result)
+    })
+
+    app.post('/cart',async(req,res)=>{
+        const item = req.body;
+        const result = await cartCollection.insertOne(item)
+        res.send(result)
+    })
+
+   
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/',(req,res)=>{
     res.send('server start now')
